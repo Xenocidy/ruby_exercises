d90edbbcf123bc7eccad2803d0b3f5618b394249
Chak Wong <jackywong3883@gmail.com>
2022-07-14 16:05:28 -0400

    Add skeleton code

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,5 +1,17 @@
 puts "Enter a string"
 input = gets.chomp
 
+# validate input
+
+# Right shift
 puts "Enter a shift factor"
 shift_factor = gets.chomp
+
+# validate shift_factor
+
+def caesar_cipher(string, shift_factor)
+    p string
+    p shift_factor
+end
+
+caesar_cipher(input, shift_factor)
