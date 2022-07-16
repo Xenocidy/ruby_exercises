b63a5a916d5f33e373ceb7d81f881656ecf2fac1
Chak Wong <jackywong3883@gmail.com>
2022-07-15 21:00:34 -0400

    Validate shift factor

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,13 +1,15 @@
 puts "Enter a string"
 input = gets.chomp
 
-# validate input
-
 # Right shift
 puts "Enter a shift factor"
-shift_factor = gets.chomp
+shift_factor = gets.chomp.to_i
 
-# validate shift_factor
+# validate shift_factor to be valid integer
+while shift_factor == 0 
+    puts "Enter a valid shift factor"
+    shift_factor = gets.chomp.to_i
+end
 
 def caesar_cipher(string, shift_factor)
     p string
