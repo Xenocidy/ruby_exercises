341fa4c195d75ccb0f5696d11c19b718b37c13e1
Chak Wong <jackywong3883@gmail.com>
2022-07-15 21:39:25 -0400

    Implement caesar cipher function

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -11,9 +11,50 @@ while shift_factor == 0
     shift_factor = gets.chomp.to_i
 end
 
-def caesar_cipher(string, shift_factor)
-    p string
-    p shift_factor
+##
+# For each character in the string, if it's a letter, shift it by the shift factor, and if it's not a
+# letter, leave it alone
+# 
+# Args:
+#   stringArr: an array of strings to be encrypted
+#   shift_factor: the amount of letters to shift the string by
+def caesar_cipher(stringArr, shift_factor)
+    answer = []
+    for i in 0..stringArr.length - 1
+
+        value = stringArr[i].ord
+
+        # Uppercase is between 65 and 90
+        if 65 <= value && value <= 90
+
+            value += shift_factor
+
+            if value > 90
+                value -= 26
+            end
+
+            value = value.chr
+
+        # Lowercase is between 97 and 122
+        elsif 97 <= value && value <= 122
+
+            value += shift_factor
+
+            if value > 122
+                value -= 26
+            end
+
+            value = value.chr
+
+        # Not a letter
+        else
+            value = value.chr
+        end
+
+        answer.push(value)
+    end
+
+    p answer.join("")
 end
 
-caesar_cipher(input, shift_factor)
+caesar_cipher(input.split(""), shift_factor)
