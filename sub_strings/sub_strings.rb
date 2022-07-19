83c064242787dd125817396076b06f3e6eee399c
Chak Wong <jackywong3883@gmail.com>
2022-07-18 20:45:59 -0400

    Implement substrings

--- sub_strings/sub_strings.rb
+++ sub_strings/sub_strings.rb
@@ -1,3 +1,27 @@
-def substrings(string, dictionary)
+dictionary = ["below","down","go","going",
+    "horn","how","howdy","it","i","low","own","part","partner","sit"]
 
+##
+# For each word in the dictionary, we check if it occurs in the string. 
+# If it does, we add it to the answer hash with the number of occurrences
+# 
+# Args:
+#   string: a string of words
+#   dictionary: an array of words to check against
+def substrings(string, dictionary)
+    string = string.downcase
+    answer = {}
+
+    for word in dictionary
+        occurrence = string.scan(word).length
+        if occurrence > 0
+            answer[word] = occurrence
+        end
+    end
+
+    p answer
 end
+
+substrings("below", dictionary)
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
